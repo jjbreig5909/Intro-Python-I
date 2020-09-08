6fd9674aff410d7229c9bba4e9a23935f6f17a5a
Jeff Breig <jjbreig@gmail.com>
2020-09-08 19:51:42 -0400

    finished dictionaries

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -46,6 +46,11 @@ print(waypoints)
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]["lon"] = -130
+waypoints[0]["name"]="not a real place"
+print(waypoints[0])
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for i in waypoints:
+    print(i)
